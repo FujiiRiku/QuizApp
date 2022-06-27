f70091433bf30e7d2c972b560a32d9bc36aaf6b5
Fujii Riku <kd1330983@st.kobedenshi.ac.jp>
2022-06-27 20:13:45 +0900

    ResultActivityとactivity_result.xmlの作成

---
+++ app/src/main/java/to/msn/wings/quizapp/ResultActivity.kt
@@ -0,0 +1,29 @@
+package to.msn.wings.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val userName = findViewById<TextView>(R.id.user_name)
+        val userScore = findViewById<TextView>(R.id.user_score)
+        val btnFinish = findViewById<Button>(R.id.btn_finish)
+
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS,0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS,0)
+
+        userName.text = intent.getStringExtra(Constants.USER_NAME)
+        userScore.text = "あなたの得点　${correctAnswers}/${totalQuestions}"
+
+        btnFinish.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+    }
+}
